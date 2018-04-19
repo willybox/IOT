d5c21955c39c4665eae0a8bf608ff3da5e5f7d4f
WillyBox <ung.willybox@gmail.com>
2018-04-19 10:57:58 +0200

    Code Arduino

---
+++ event_sender/event_sender.ino
@@ -0,0 +1,67 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMesh.h>
+
+unsigned int request_i = 0;
+unsigned int response_i = 0;
+
+String manageRequest(String request);
+
+/* Create the mesh node object */
+ESP8266WiFiMesh mesh_node = ESP8266WiFiMesh(ESP.getChipId(), manageRequest);
+
+/**
+   Callback for when other nodes send you data
+
+   @request The string received from another node in the mesh
+   @returns The string to send back to the other node
+*/
+String manageRequest(String request) {
+  /* Print out received message */
+  Serial.print("received: ");
+  Serial.println(request);
+
+  /* return a string to send back */
+  char response[60];
+  sprintf(response, "Hello world response #%d from Mesh_Node%d.", response_i++, ESP.getChipId());
+  return response;
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  //configure pin 2 as an input and enable the internal pull-up resistor
+  pinMode(D2, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  delay(10);
+
+  Serial.println();
+  Serial.println();
+  Serial.println("Setting up mesh node...");
+
+  /* Initialise the mesh node */
+  mesh_node.begin();
+}
+
+void loop() {
+  mesh_node.acceptRequest();
+  //read the pushbutton value into a variable
+  int sensorVal = digitalRead(D2);
+
+  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (sensorVal == HIGH) {
+    digitalWrite(LED_BUILTIN, LOW);
+    
+  } else {
+     Serial.println("CLICK");
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    /* Scan for other nodes and send them a message */
+    char request[60];
+    sprintf(request, "Hello world request #%d from Mesh_Node%d.", request_i++, ESP.getChipId());
+    mesh_node.attemptScan(request);
+  }
+
+}
